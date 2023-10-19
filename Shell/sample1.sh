961e4e44fd72fa883ff8794d75828c857c8155bd
yonezawa <yonetti20@gmail.com>
2023-10-19 18:26:10 +0900

    不要なファイルを消した

--- Shell/sample1.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for line in $(printenv)
-do
-    echo ${line}
-done
--- powershell/kai.ps1
+++
@@ -1,8 +0,0 @@
-﻿Param($Args1)
-
-$sum = $Args1
-for($i = $Args1 - 1; $i -gt 0; $i --) 
-{
-    $sum *= $i;
-}
-Write-Host $sum
