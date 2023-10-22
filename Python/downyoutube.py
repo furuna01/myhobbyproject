c24ae0707ad847eb9be7f4567ca0dbe33f15bce0
yonezawa <yonetti20@gmail.com>
2023-10-22 17:42:20 +0900

    I revise the program to download YouTube video, because library I used was outdated

--- Python/downyoutube.py
+++ Python/downyoutube.py
@@ -1,6 +1,16 @@
-from __future__ import unicode_literals
-import youtube_dl
-ydl_opts = {}
-url = "https://www.youtube.com/watch?v=eZoMo4z-giwS"
-with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-    ydl.download([url])
+import sys
+
+args = sys.argv
+
+for str in args:
+    print(str)
+
+Url = ""
+if len(args) == 1:
+    Url = "https://www.youtube.com/watch?v=4IKLtDBL9lQ&t=297s"
+elif len(args) == 2:
+    url = args[1]
+
+from yt_dlp import YoutubeDL
+with YoutubeDL() as ydl:
+    result = ydl.download([url])
